f3ad965a20c8b5e572ede60611d2d9e34329385c
Rain Jocas <rainjocas@gmail.com>
2025-04-28 19:27:24 -0400

    Layout test

--- costumeDesign/src/components/Person.tsx
+++ costumeDesign/src/components/Person.tsx
@@ -5,10 +5,12 @@ import React from "react";
  */
 interface personProps {
     name: string,
+    
     workName?: string,
     workClothingimg?: string,
     workClothingimgAlt?: string,
     workClothingPeiceList?: string,
+
     musicName?: string,
     musicStatement?: string,
     musicimg1?: string,
@@ -16,6 +18,7 @@ interface personProps {
     musicimg2?: string,
     musicimg2Alt?: string,
     musicPeiceList?: string,
+
     constructionName?: string,
     constructionStatement?: string,
     constructionimg1?: string,
@@ -24,6 +27,18 @@ interface personProps {
     constructionimg2Alt?: string,
     constructionimg3?: string,
     constructionimg3Alt?: string,
+
+    productionName?: string
+    productionStatement?: string
+    productionimg1?: string
+    productionimg1Alt?: string,
+    productionimg2?: string
+    productionimg2Alt?: string,
+    productionimg3?: string,
+    productionimg3Alt?: string,
+    productionimg4?: string,
+    productionimg4Alt?: string
+
     finalStatement?: string,
     finalimg1?: string,
     finalimg1Alt?: string,
@@ -41,14 +56,22 @@ interface personProps {
  * 
  */
 const Person: React.FC<personProps> = ({ 
-    name, workName, workClothingimg, workClothingimgAlt, workClothingPeiceList,
+    name,
+    
+    workName, workClothingimg, workClothingimgAlt, workClothingPeiceList,
+    
     musicName, musicStatement, musicimg1, musicimg1Alt, musicimg2,
-    musicimg2Alt, musicPeiceList, constructionName, constructionStatement,
-    constructionimg1, constructionimg1Alt, constructionimg2,
-    constructionimg2Alt, constructionimg3, constructionimg3Alt,
-    finalStatement, finalimg1, finalimg1Alt, finalimg2, finalimg2Alt,
-    finalimg3, finalimg3Alt, finalimg4, finalimg4Alt, finalimg5,
-    finalimg5Alt,
+    musicimg2Alt, musicPeiceList,
+    
+    constructionName, constructionStatement, constructionimg1, constructionimg1Alt,
+    constructionimg2, constructionimg2Alt, constructionimg3, constructionimg3Alt,
+    
+    productionName, productionStatement, productionimg1, productionimg1Alt,
+    productionimg2, productionimg2Alt, productionimg3, productionimg3Alt, productionimg4,
+    productionimg4Alt,
+
+    finalStatement, finalimg1, finalimg1Alt, finalimg2, finalimg2Alt, finalimg3,
+    finalimg3Alt, finalimg4, finalimg4Alt, finalimg5, finalimg5Alt,
 }) => {
     return (
         <div className="mx-20 my-10" role="region" aria-label={name}>
@@ -66,30 +89,46 @@ const Person: React.FC<personProps> = ({
                     {musicStatement}
                 <h3>Costume Sketches</h3>
                     <div className="flex flex-row items-center justify-center">
-                        <img src = {musicimg1} alt = {musicimg1Alt}/>
-                        <img src = {musicimg2} alt = {musicimg2Alt}/>
+                        <img className = "w-1/2" src = {musicimg1} alt = {musicimg1Alt}/>
+                        <img className = "w-1/2" src = {musicimg2} alt = {musicimg2Alt}/>
                     </div>
                 <h3>Piece List</h3>
                     <img src = {musicPeiceList} alt = {"Peice List"}/>
             <h2>{constructionName}</h2>
                 <h3>Artist Statement</h3>
+                    {productionStatement}
+                <h3>Costume Sketches</h3>
+                    <div className="flex flex-row items-center justify-center">
+                        <img className = "w-1/3" src = {constructionimg1} alt = {constructionimg1Alt}/>
+                        <img className = "w-1/3" src = {constructionimg2} alt = {constructionimg2Alt}/>
+                        <img className = "w-1/3" src = {constructionimg3} alt = {constructionimg3Alt}/>
+                    </div>
+            <h2>{productionName}</h2>
+                <h3>Artist Statement</h3>
                     {constructionStatement}
                 <h3>Costume Sketches</h3>
                     <div className="flex flex-row items-center justify-center">
-                        <img src = {constructionimg1} alt = {constructionimg1Alt}/>
-                        <img src = {constructionimg2} alt = {constructionimg2Alt}/>
-                        <img src = {constructionimg3} alt = {constructionimg3Alt}/>
+                        <img className = "w-1/4" src = {productionimg1} alt = {productionimg1Alt}/>
+                        <img className = "w-1/4" src = {productionimg2} alt = {productionimg2Alt}/>
+                        <img className = "w-1/4" src = {productionimg3} alt = {productionimg3Alt}/>
+                        <img className = "w-1/4" src = {productionimg4} alt = {productionimg4Alt}/>
                     </div>
+                    <img src = {productionimg1} alt = {productionimg1Alt}/>
+                        <img src = {productionimg2} alt = {productionimg2Alt}/>
+                        <img src = {productionimg3} alt = {productionimg3Alt}/>
+                        <img src = {productionimg4} alt = {productionimg4Alt}/>
             <h2>Midsummer Night's Dream</h2>
                 <h3>Artist Statement</h3>
                     <p>{finalStatement}</p>
                 <h3>Costume Sketches</h3>
                     <div className="flex flex-row items-center justify-center">
-                        <img src = {finalimg1} alt = {finalimg1Alt}/>
-                        <img src = {finalimg2} alt = {finalimg2Alt}/>
-                        <img src = {finalimg3} alt = {finalimg3Alt}/>
-                        <img src = {finalimg4} alt = {finalimg4Alt}/>
-                        <img src = {finalimg5} alt = {finalimg5Alt}/>
+                        <img className = "w-1/2" src = {finalimg1} alt = {finalimg1Alt}/>
+                        <img className = "w-1/2" src = {finalimg2} alt = {finalimg2Alt}/>
+                    </div>
+                    <div className="flex flex-row items-center justify-center">
+                        <img className = "w-1/3" src = {finalimg3} alt = {finalimg3Alt}/>
+                        <img className = "w-1/3" src = {finalimg4} alt = {finalimg4Alt}/>
+                        <img className = "w-1/3" src = {finalimg5} alt = {finalimg5Alt}/>
                     </div>
         </div>  
   );
